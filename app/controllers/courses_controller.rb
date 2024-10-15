53f75eb16dd1f5cfa30f3d8d2a2468db575af33e
dayrajefil <dayrajefil@gmail.com>
2024-10-14 22:58:00 -0300

    20241014-001 feature: Implement course creation, editing, and deletion endpoints     - Added endpoints for creating, editing, and deleting courses.     - Implemented validations for required fields in the course model.     - Updated routes to handle CRUD operations for courses.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,41 @@
 class CoursesController < ApplicationController
   def index
     courses = Course.where('end_date >= ?', Date.today)
-    render json: courses #, include: :videos
+    render json: courses
+  end
+
+  def show
+    course = Course.find(params[:id])
+    render json: course
+  end
+
+  def create
+    course = Course.new(course_params)
+    if course.save
+      render json: { course: course, success: 'Criado com sucesso!'}, status: :created
+    else
+      render json: { errors: format_errors(course.errors) }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    course = Course.find(params[:id])
+    if course.update(course_params)
+      render json: { course: course, success: 'Atualizado com sucesso!'}, status: :ok
+    else
+      render json: { errors: format_errors(course.errors) }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title, :description, :start_date, :end_date)
+  end
+
+  def format_errors(errors)
+    errors.map do |error|
+      { attribute: error.attribute, message: error.message }
+    end
   end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,17 @@
 class Course < ApplicationRecord
+  has_many :videos, dependent: :destroy
+
+  validates :title, :description, :start_date, :end_date, presence: true
+
+  validate :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    return if start_date.blank? || end_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, "deve ser maior ou igual à data de início.")
+    end
+  end
 end
